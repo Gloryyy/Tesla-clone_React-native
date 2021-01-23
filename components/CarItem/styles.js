e978a1b2f678621e58a3e8f2d3552d1890f36a60
gloryyy <majdwarrrak@gmail.com>
2021-01-23 02:11:56 +0100

    Car Item component

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -18,11 +18,19 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#5c5e62",
   },
+  subtitleCTA: {
+    textDecorationLine: "underline",
+  },
   image: {
     width: "100%",
     height: "100%",
     position: "absolute",
     resizeMode: "cover",
+  },
+  buttonsContainer: {
+    position: "absolute",
+    bottom: 50,
+    width: "100%",
   },
 });
 
